4cdde229a7122a3415dfc04bbfa8ecc624bf81d4
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-04-14 01:45:04 +0300

    LR8.N1.1 "Create DB Prolog family"

---
+++ family_tree.pl
@@ -0,0 +1,24 @@
+man(anatoliy).
+man(dimitriy).
+man(vlad).
+man(kirill).
+man(mefodiy).
+
+woman(vladina).
+woman(galya).
+woman(sveta).
+woman(zoya).
+woman(katrin).
+
+dite(dimitriy, anatoliy).
+dite(dimitriy, galya).
+dite(vladina, anatoliy).
+dite(vladina, galya).
+dite(kirill, dimitriy).
+dite(mefodiy, dimitriy).
+dite(kirill, sveta).
+dite(mefodiy, sveta).
+dite(zoya, vlad).
+dite(zoya, vladina).
+dite(katrin, vlad).
+dite(katrin, vladina).
