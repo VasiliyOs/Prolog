447230394536406a4ad3d5ca0cf0f5d81413b444
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-04-21 02:42:38 +0300

    LR9.N1.2 "Add predicate fact_down and fact_up"

--- Lab9.pl
+++ Lab9.pl
@@ -7,3 +7,21 @@ max(A, B, A) :- A >= B.
 max(A, B, B) :- A < B.
 
 
+fact_down(0, 1).
+fact_down(N, X) :- 
+    N > 0,
+    N1 is N - 1,
+    fact_down(N1, X1),
+    X is N * X1.
+
+
+
+fact_up(N, X) :- 
+    fact_up(N, 1, X).
+
+fact_up(0, Acc, Acc). 
+fact_up(N, Acc, X) :- 
+    N > 0,
+    N1 is N - 1,
+    Acc1 is Acc * N,
+    fact_up(N1, Acc1, X).
