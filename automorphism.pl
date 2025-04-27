516d6c73f7406276d3cfe7542a1a0172e41387b9
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-04-28 00:10:19 +0300

    IZ2 "Add predicate permutation for find all permutations"

--- automorphism.pl
+++ automorphism.pl
@@ -6,3 +6,8 @@ edge(4, 1).
 vertices(Vertices) :-
     findall(X, (edge(X, _); edge(_, X)), AllEdges),
     sort(AllEdges, Vertices).
+
+permutation([], []).
+permutation(List, [H|Perm]) :-
+    select(H, List, Rest),
+    permutation(Rest, Perm).
